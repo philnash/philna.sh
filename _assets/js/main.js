bf81c468c7b46e0d6ed66ad7b4f8ae7535e7337e
Phil Nash <philnash@gmail.com>
2017-02-14 02:14:00 +0000

    Attempt at implementing Web Share API.

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,3 +1,23 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register("/sw.js");
+  navigator.serviceWorker.register('/sw.js');
 }
+
+document.addEventListener('DOMContentLoaded', function(event) {
+  var shareLinks = [].slice.call(document.querySelectorAll('.share'));
+  shareLinks.forEach(function(link) {
+    link.addEventListener('click', function (event) {
+      if (typeof navigator.share !== 'undefined') {
+        var canonicalElement = document.querySelector('link[rel=canonical]');
+        if(canonicalElement !== undefined) {
+          var url = canonicalElement.href;
+        } else {
+          var url = window.location.href;
+        }
+        var pageTitle = document.querySelector('.post-title').textContent;
+        return navigator.share({ title: pageTitle, url: url })
+          .then(function() { return false; })
+          .catch(function(err) { return true; });
+      }
+    })
+  })
+});
