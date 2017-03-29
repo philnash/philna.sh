eeee5c3dceda50a53a363850bcad5e26bc432547
Phil Nash <philnash@gmail.com>
2017-03-29 11:28:06 +0100

    I think this offline stuff is nearly ready

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,22 +1,50 @@
-document.addEventListener('DOMContentLoaded', function(event) {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js');
+function getCanonicalUrl() {
+  var canonicalElement = document.querySelector('link[rel=canonical]');
+  return (canonicalElement !== undefined) ? canonicalElement.href : window.location.href;
+}
+
+function getPageTitle() {
+  var path = window.location.pathname;
+  var h1 = document.querySelector('h1');
+  if (path === '/') { return "Home" }
+  if (path.indexOf('/page/') > 0) {
+    var pageNumber = path.split('/page/').reverse()[0].split('/')[0];
+    return h1.textContent + ": page " + pageNumber;
   }
+  return h1.textContent;
+}
+
+function getGroupNumber() {
+  var path = window.location.pathname;
+  if (path === '/') { return 1; }
+  if (path === '/blog/' || path.match(/\/blog\/page\/\d/)) { return 2; }
+  return 3;
+}
+
+function openDb() {
+  return idb.open('site', 1, function(upgradeDb) {
+    upgradeDb.createObjectStore('pages', { keyPath: 'url' });
+  });
+}
+
+function swSupport() {
+  return 'serviceWorker' in navigator;
+}
+
+if (swSupport()) {
+  navigator.serviceWorker.register('/sw.js');
+}
+
+document.addEventListener('DOMContentLoaded', function(event) {
+  var url = getCanonicalUrl();
+  var pageTitle = getPageTitle();
 
   var shareLinks = [].slice.call(document.querySelectorAll('.share'));
   shareLinks.forEach(function(link) {
     link.addEventListener('click', function(event) {
       if (typeof navigator.share !== 'undefined') {
         event.preventDefault();
-        var canonicalElement = document.querySelector('link[rel=canonical]');
-        if (canonicalElement !== undefined) {
-          var url = canonicalElement.href;
-        } else {
-          var url = window.location.href;
-        }
-        var pageTitle = document.querySelector('.post-title').textContent;
-        navigator
-          .share({ title: pageTitle, url: url })
+        navigator.share({ title: pageTitle, url: url })
           .then(function() {
             // track successful share
             ga('send', {
@@ -46,6 +74,21 @@ document.addEventListener('DOMContentLoaded', function(event) {
           eventLabel: 'click'
         });
       }
-    });
+    })
   });
+
+  if (swSupport()) {
+    navigator.serviceWorker.ready
+      .then(function(reg) {
+        openDb()
+          .then(function(db) {
+          var objectStore = db.transaction('pages', 'readwrite').objectStore('pages');
+          return objectStore.put({
+            url: url,
+            title: pageTitle,
+            group: getGroupNumber()
+          });
+        });
+      });
+  }
 });
--- sw.js
+++ sw.js
@@ -1,17 +1,31 @@
 ---
 ---
 {% capture digest_paths %}{% for asset in assets %}{% unless asset[0] contains "/" %}{% unless asset[0] contains "merriweather" %}{% unless asset[0] contains "raleway" %}{{ asset[1].digest_path }},{% endunless %}{% endunless %}{% endunless %}{% endfor %}{% endcapture %}
+var version = "v{{ site.sw_cache_version }}-";
 
-var cacheName = "assets-{{ digest_paths | md5 }}";
+var staticCacheName = version + "assets-{{ digest_paths | md5 }}";
+var staticAssets = ['{{ digest_paths | prepend: "/assets/" | split: "," | join: "', '/assets/" }}'];
+
+var pageCacheName = version + "pages";
+var offlinePages = ['/', '/blog/', '/offline/'];
+var currentCaches = [staticCacheName, pageCacheName];
 
 self.addEventListener("install", function(event) {
   event.waitUntil(
-    caches.open(cacheName)
-      .then(function(cache) {
-        return cache.addAll([
-          '{{ digest_paths | prepend: "/assets/" | split: "," | join: "', '/assets/" }}'
-        ]);
-      })
+    Promise.all([
+      caches
+        .open(staticCacheName)
+        .then(function(cache) {
+          return cache.addAll(staticAssets);
+        }),
+      caches
+        .open(pageCacheName)
+        .then(function(cache) {
+          return cache.addAll(offlinePages);
+        })
+    ]).then(function() {
+      self.skipWaiting();
+    })
   );
 });
 
@@ -19,18 +33,25 @@ self.addEventListener("activate", function(event) {
   event.waitUntil(
     caches.keys().then(function(names) {
       return Promise.all(names.filter(function(name) {
-        return name !== cacheName;
+        return currentCaches.indexOf(name) === -1;
       }).map(function(name) {
         return caches.delete(name);
       }));
+    }).then(function() {
+      self.clients.claim();
     })
   );
 });
 
 self.addEventListener("fetch", function(event) {
   var url = new URL(event.request.url);
-  if (url.pathname.match(/^\/assets\//)) {
-    event.respondWith(returnFromCacheOrFetch(event.request, cacheName));
+  if (url.pathname.match(/^\/(assets|images)\//)) {
+    event.respondWith(returnFromCacheOrFetch(event.request, staticCacheName));
+  }
+  if (event.request.mode === 'navigate' ||
+      event.request.headers.get('Accept').indexOf('text/html') !== -1) {
+    // cache then network
+    event.respondWith(cacheThenNetwork(event.request, pageCacheName));
   }
 })
 
@@ -46,8 +67,37 @@ function returnFromCacheOrFetch(request, cacheName) {
     // return the cached response if we have it, otherwise the result of the fetch.
     return cacheResponse || fetch(request).then(function(fetchResponse) {
       // Cache the updated file and then return the response
-      cache.put(request, fetchResponse.clone());
+      if (fetchResponse.ok) {
+        cache.put(request, fetchResponse.clone());
+      }
       return fetchResponse;
     });
+  });
+}
+
+function cacheThenNetwork(request, cacheName) {
+  var cachePromise = caches.open(cacheName);
+  var matchPromise = cachePromise.then(function(cache) {
+    return cache.match(request);
+  });
+
+  return Promise.all([cachePromise, matchPromise]).then(function(responses) {
+    var cache = responses[0];
+    var cacheResponse = responses[1];
+    if (cacheResponse) {
+      fetch(request)
+        .then(function(fetchResponse) {
+          cache.put(request, fetchResponse);
+        });
+      return cacheResponse;
+    } else {
+      return fetch(request)
+        .then(function(fetchResponse) {
+          cache.put(request, fetchResponse.clone());
+          return fetchResponse;
+        }).catch(function() {
+          return caches.match('/offline/');
+        });
+    }
   });
 }
