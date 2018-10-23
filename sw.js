ba798a2d5d8364fc7c1dae1819cbd8ef103c8b67
Phil Nash <philnash@gmail.com>
2018-10-23 22:34:44 +1100

    Better updates for the service worker.

--- sw.js
+++ sw.js
@@ -6,11 +6,11 @@ var version = "v{{ site.sw_cache_version }}-";
 var staticCacheName = version + "assets-{{ digest_paths | md5 }}";
 var staticAssets = ['/manifest.json', '{{ digest_paths | split: "," | join: "', '" }}'];
 
-var pageCacheName = version + "pages";
+var pageCacheName = version + 'pages';
 var offlinePages = ['/', '/blog/', '/offline/'];
 var currentCaches = [staticCacheName, pageCacheName];
 
-self.addEventListener("install", function(event) {
+self.addEventListener('install', function(event) {
   event.waitUntil(
     Promise.all([
       cacheAllIn(staticAssets, staticCacheName),
@@ -21,7 +21,7 @@ self.addEventListener("install", function(event) {
   );
 });
 
-self.addEventListener("activate", function(event) {
+self.addEventListener('activate', function(event) {
   event.waitUntil(
     deleteOldCaches(currentCaches).then(function() {
       self.clients.claim();
@@ -29,7 +29,7 @@ self.addEventListener("activate", function(event) {
   );
 });
 
-self.addEventListener("fetch", function(event) {
+self.addEventListener('fetch', function(event) {
   var url = new URL(event.request.url);
   if (url.pathname.match(/^\/((assets|images)\/|manifest.json$)/)) {
     if (event.request.headers.get('range')) {
@@ -38,64 +38,78 @@ self.addEventListener("fetch", function(event) {
       event.respondWith(returnFromCacheOrFetch(event.request, staticCacheName));
     }
   }
-  if (event.request.mode === 'navigate' ||
-      event.request.headers.get('Accept').indexOf('text/html') !== -1) {
+  if (
+    event.request.mode === 'navigate' ||
+    event.request.headers.get('Accept').indexOf('text/html') !== -1
+  ) {
     // cache then network
     event.respondWith(cacheThenNetwork(event.request, pageCacheName));
   }
 });
 
 function returnRangeRequest(request, cacheName) {
-  return caches.open(cacheName)
-  .then(function(cache) {
-    return cache.match(request.url);
-  }).then(function(res) {
-    if (!res) {
-      return fetch(request)
-      .then(res => {
-        const clonedRes = res.clone();
-        return caches.open(cacheName).then(cache => cache.put(request, clonedRes)).then(() => res);
-      })
-      .then(res => {
-        return res.arrayBuffer();
-      });
-    }
-    return res.arrayBuffer();
-  }).then(function(ab) {
-    const bytes = /^bytes\=(\d+)\-(\d+)?$/g.exec(request.headers.get('range'));
-    let start, end;
-    if (bytes) {
-      start = Number(bytes[1]);
-      end = Number(bytes[2]) || ab.byteLength-1;
-    } else {
-      start = 0;
-      end = ab.byteLength-1;
-    }
-    const range = ['Content-Range', `bytes ${start}-${end}/${ab.byteLength}`];
-    return new Response(
-      ab.slice(start, end+1),
-      {
-        status: 206,
-        statusText: 'Partial Content',
-        headers: [range]
-      });
-  })
+  return caches
+    .open(cacheName)
+    .then(function(cache) {
+      return cache.match(request.url);
+    })
+    .then(function(res) {
+      if (!res) {
+        return fetch(request)
+          .then(res => {
+            const clonedRes = res.clone();
+            return caches
+              .open(cacheName)
+              .then(cache => cache.put(request, clonedRes))
+              .then(() => res);
+          })
+          .then(res => {
+            return res.arrayBuffer();
+          });
+      }
+      return res.arrayBuffer();
+    })
+    .then(function(arrayBuffer) {
+      const bytes = /^bytes\=(\d+)\-(\d+)?$/g.exec(
+        request.headers.get('range')
+      );
+      if (bytes) {
+        const start = Number(bytes[1]);
+        const end = Number(bytes[2]) || arrayBuffer.byteLength - 1;
+        return new Response(arrayBuffer.slice(start, end + 1), {
+          status: 206,
+          statusText: 'Partial Content',
+          headers: [
+            ['Content-Range', `bytes ${start}-${end}/${arrayBuffer.byteLength}`]
+          ]
+        });
+      } else {
+        return new Response(null, {
+          status: 416,
+          statusText: 'Range Not Satisfiable',
+          headers: [['Content-Range', `*/${arrayBuffer.byteLength}`]]
+        });
+      }
+    });
 }
+
 
 function cacheAllIn(paths, cacheName) {
   return caches.open(cacheName).then(function(cache) {
     return cache.addAll(paths);
-  })
+  });
 }
 
 function deleteOldCaches(currentCaches) {
   return caches.keys().then(function(names) {
     return Promise.all(
-      names.filter(function(name) {
-        return currentCaches.indexOf(name) === -1;
-      }).map(function(name) {
-        return caches.delete(name);
-      })
+      names
+        .filter(function(name) {
+          return currentCaches.indexOf(name) === -1;
+        })
+        .map(function(name) {
+          return caches.delete(name);
+        })
     );
   });
 }
@@ -104,7 +118,7 @@ function openCacheAndMatchRequest(cacheName, request) {
   var cachePromise = caches.open(cacheName);
   var matchPromise = cachePromise.then(function(cache) {
     return cache.match(request);
-  })
+  });
   return [cachePromise, matchPromise];
 }
 
@@ -112,29 +126,33 @@ function cacheSuccessfulResponse(cache, request, response) {
   if (response.ok) {
     return cache.put(request, response.clone()).then(() => {
       return response;
-    })
+    });
   } else {
     return response;
   }
 }
 
 function returnFromCacheOrFetch(request, cacheName) {
-  return Promise.all(openCacheAndMatchRequest(cacheName, request))
-    .then(function(responses) {
+  return Promise.all(openCacheAndMatchRequest(cacheName, request)).then(
+    function(responses) {
       var cache = responses[0];
       var cacheResponse = responses[1];
       // return the cached response if we have it, otherwise the result of the fetch.
-      return cacheResponse || fetch(request).then(function(fetchResponse) {
-        // Cache the updated file and then return the response
-        cacheSuccessfulResponse(cache, request, fetchResponse);
-        return fetchResponse;
-      });
-    });
+      return (
+        cacheResponse ||
+        fetch(request).then(function(fetchResponse) {
+          // Cache the updated file and then return the response
+          cacheSuccessfulResponse(cache, request, fetchResponse);
+          return fetchResponse;
+        })
+      );
+    }
+  );
 }
 
 function cacheThenNetwork(request, cacheName) {
-  return Promise.all(openCacheAndMatchRequest(cacheName, request))
-    .then(function(responses) {
+  return Promise.all(openCacheAndMatchRequest(cacheName, request)).then(
+    function(responses) {
       var cache = responses[0];
       var cacheResponse = responses[1];
       if (cacheResponse) {
@@ -155,12 +173,14 @@ function cacheThenNetwork(request, cacheName) {
           .then(function(fetchResponse) {
             cacheSuccessfulResponse(cache, request, fetchResponse);
             return fetchResponse;
-          }).catch(function() {
+          })
+          .catch(function() {
             // Offline, so return the offline page.
             return caches.match('/offline/');
           });
       }
-    });
+    }
+  );
 }
 
 function refresh(response) {
