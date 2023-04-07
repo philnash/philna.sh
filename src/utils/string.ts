ab5ce15d4d93a5596f1d5954f3ad249119e49ba8
Phil Nash <philnash@gmail.com>
2023-04-07 15:23:02 +1000

    Truncate post body as post description

---
+++ src/utils/string.ts
@@ -0,0 +1,11 @@
+import { toString } from "mdast-util-to-string";
+import { fromMarkdown } from "mdast-util-from-markdown";
+
+export function truncate(text: string, numberOfCharacters: number) {
+  const plainText = toString(fromMarkdown(text));
+  if (plainText.length <= numberOfCharacters) {
+    return plainText;
+  }
+  const subString = plainText.slice(0, numberOfCharacters - 1);
+  return `${subString.slice(0, subString.lastIndexOf(" "))}...`;
+}
