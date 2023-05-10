c259a9bd801b2bb3658f42b8ca39810188a9f01f
Phil Nash <philnash@gmail.com>
2023-05-10 12:19:14 +1000

    Adds upcoming appearances. Fixes display of appearances

--- src/utils/appearances.ts
+++ src/utils/appearances.ts
@@ -1,7 +1,7 @@
 import type { Appearance } from "../data/appearances.yml";
 
 export function displayDate(startDate: Date, endDate?: Date) {
-  if (endDate) {
+  if (endDate && startDate.getTime() !== endDate.getTime()) {
     const start = startDate.toLocaleDateString("en", {
       month: "short",
       day: "numeric",
@@ -21,18 +21,44 @@ export function displayDate(startDate: Date, endDate?: Date) {
   }
 }
 
-export function nextThreeAppearances(appearances: Appearance[], today: Date) {
+export function getToday() {
+  const now = new Date();
+  return new Date(now.getFullYear(), now.getMonth(), now.getDate());
+}
+
+export function upcomingAppearances(appearances: Appearance[], today: Date) {
   return appearances
-    .filter((appearance) => appearance.event.start_date >= today)
-    .sort((a, b) => a.event.start_date.getTime() - b.event.start_date.getTime())
-    .slice(0, 3);
+    .filter((appearance) => {
+      if (appearance.event.end_date) {
+        return appearance.event.end_date >= today;
+      }
+      return appearance.event.start_date >= today;
+    })
+    .sort(
+      (a, b) => a.event.start_date.getTime() - b.event.start_date.getTime()
+    );
+}
+
+export function pastAppearances(appearances: Appearance[], today: Date) {
+  return appearances
+    .filter((appearance) => {
+      if (appearance.event.end_date) {
+        return appearance.event.end_date < today;
+      }
+      return appearance.event.start_date < today;
+    })
+    .sort(
+      (a, b) => b.event.start_date.getTime() - a.event.start_date.getTime()
+    );
+}
+
+export function nextThreeAppearances(appearances: Appearance[], today: Date) {
+  return upcomingAppearances(appearances, today).slice(0, 3);
 }
 
 export function previousThreeAppearances(
   appearances: Appearance[],
   today: Date
 ) {
-  return appearances
-    .filter((appearance) => appearance.event.start_date < today)
-    .slice(0, 3);
+  return pastAppearances(appearances, today).slice(0, 3);
 }
