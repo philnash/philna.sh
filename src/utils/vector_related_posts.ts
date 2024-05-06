2af47d67e4d602ddf801e8721a37b665a7e1a04c
Phil Nash <philnash@gmail.com>
2024-05-06 18:45:26 +1000

    Refactor related posts

--- src/utils/vector_related_posts.ts
+++ src/utils/vector_related_posts.ts
@@ -1,46 +1,56 @@
+import { DataAPIClient } from "@datastax/astra-db-ts";
 import type { StrictFilter } from "@datastax/astra-db-ts";
-import { connect, getBlogCollection } from "./astradb";
-import type { BlogEmbeddingDoc } from "./astradb";
 import { getEmbeddings } from "./openai";
 
-export interface BlogEmbeddingData {
-  slug: string;
-  title: string;
-  path: string;
-  body: string;
-};
+import { getEntry } from "astro:content";
+import type { CollectionEntry } from "astro:content";
 
-export async function findOrCreateBlogEmbedding({ slug, title, path, body }: BlogEmbeddingData) {
-  const db = connect();
-  const collection = getBlogCollection(db);
-  const item = await collection.findOne({ _id: slug });
+const { ASTRADB_APP_TOKEN, ASTRADB_ENDPOINT } = import.meta.env;
+const COLLECTION_NAME = "philnash_blog";
+
+interface BlogEmbeddingDoc {
+  _id: string;
+  $vector: number[];
+}
+interface BlogEmbeddingData {
+  slug: string;
+  body: string;
+}
+
+const astraDb = new DataAPIClient(ASTRADB_APP_TOKEN).db(ASTRADB_ENDPOINT);
+const blogCollection = astraDb.collection<BlogEmbeddingDoc>(COLLECTION_NAME);
+
+async function findOrCreateBlogEmbedding({ slug, body }: BlogEmbeddingData) {
+  const item = await blogCollection.findOne({ _id: slug });
   if (item) {
     return item.$vector;
   }
   // create embeddings for blog post content with OpenAI API
   const embeddings = await getEmbeddings(body);
   // save embeddings to Astradb
-  await collection.insertOne({
-    _id: slug,
-    $vector: embeddings,
-    title: title,
-    path: path,
-  });
+  await blogCollection.insertOne({ _id: slug, $vector: embeddings });
   return embeddings;
 }
 
-export async function getRelatedPosts(slug: string, embeddings: number[]) {
-  const filter: StrictFilter<BlogEmbeddingDoc> = {
-    _id: { $ne: slug }
-  }
-  const options = {
-    sort: {
-      $vector: embeddings,
-    },
-    limit: 4,
-  };
-  const db = connect();
-  const blogCollection = getBlogCollection(db);
-  const results = await blogCollection.find(filter, options);
-  return results.toArray();
+function isPost(
+  post: CollectionEntry<"blog"> | undefined
+): post is CollectionEntry<"blog"> {
+  return typeof post !== "undefined";
+}
+
+export async function getRelatedPosts({
+  slug,
+  body,
+}: BlogEmbeddingData): Promise<CollectionEntry<"blog">[]> {
+  const embeddings = await findOrCreateBlogEmbedding({ slug, body });
+
+  const filter: StrictFilter<BlogEmbeddingDoc> = { _id: { $ne: slug } };
+  const options = { sort: { $vector: embeddings }, limit: 4 };
+
+  const results = await blogCollection.find(filter, options).toArray();
+  const posts = await Promise.all(
+    results.map((result) => getEntry({ collection: "blog", slug: result._id }))
+  );
+
+  return posts.filter(isPost);
 }
