6ba020311ce075718c64b83e16d4c2418ad1670c
Phil Nash <philnash@gmail.com>
2024-05-13 20:39:09 +1000

    Remove logs, type env

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,15 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 /// <reference types="./data/appearances.yml.d.ts" />
+
+declare namespace App {
+  interface Locals {
+    runtime: {
+      env: {
+        RESEND_API_KEY: string;
+        RESEND_TO_EMAIL: string;
+        RESEND_FROM_EMAIL: string;
+      };
+    };
+  }
+}
