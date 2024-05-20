e519b2a9f2ae528127d6914bf67e7705492d3f9d
Phil Nash <philnash@gmail.com>
2024-05-20 10:44:23 +1000

    Replace astro-critters with @playform/inline

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,34 +1,30 @@
 import { defineConfig, sharpImageService } from "astro/config";
 import yaml from "@rollup/plugin-yaml";
 import { DOMAIN } from "./src/consts";
-import critters from "astro-critters";
 import cloudflare from "@astrojs/cloudflare";
+import playformInline from "@playform/inline";
 
 // https://astro.build/config
 export default defineConfig({
   site: `https://${DOMAIN}`,
-  integrations: [critters({
-    critters: {
-      preload: "media"
-    }
-  })],
+  integrations: [playformInline({ Critters: { preload: "media" } })],
   vite: {
     plugins: [yaml()],
     ssr: {
-      external: ["@datastax/astra-db-ts", "openai"]
-    }
+      external: ["@datastax/astra-db-ts", "openai"],
+    },
   },
   image: {
-    service: sharpImageService()
+    service: sharpImageService(),
   },
   markdown: {
     shikiConfig: {
-      theme: "slack-dark"
-    }
+      theme: "slack-dark",
+    },
   },
   compressHTML: true,
   output: "hybrid",
   adapter: cloudflare({
-    imageService: "compile"
-  })
+    imageService: "compile",
+  }),
 });
