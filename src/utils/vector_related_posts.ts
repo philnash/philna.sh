fb92315307994acf1cf8625a43fcb4f9b6d530fe
Phil Nash <philnash@gmail.com>
2024-05-29 16:50:06 +1000

    Updates to vectorize

--- src/utils/vector_related_posts.ts
+++ src/utils/vector_related_posts.ts
@@ -1,46 +1,26 @@
 import { DataAPIClient } from "@datastax/astra-db-ts";
-import { getEmbeddings } from "./openai";
 
 import { getEntry } from "astro:content";
 import type { CollectionEntry } from "astro:content";
+import stringByteSlice from "string-byte-slice";
 
 const { ASTRADB_APP_TOKEN, ASTRADB_ENDPOINT } = import.meta.env;
-const COLLECTION_NAME = "philnash_blog";
+const COLLECTION_NAME = "blog";
 
 type BlogEmbeddingDoc = {
   _id: string;
+  $vectorize: string;
   $vector: number[];
 };
 type BlogEmbeddingData = {
   slug: string;
   body: string;
-  regenerateVectors: boolean;
 };
 
 const astraDb = new DataAPIClient(ASTRADB_APP_TOKEN).db(ASTRADB_ENDPOINT);
-const blogCollection = astraDb.collection<BlogEmbeddingDoc>(COLLECTION_NAME);
-
-async function findOrCreateBlogEmbedding({
-  slug,
-  body,
-  regenerateVectors,
-}: BlogEmbeddingData) {
-  const item = await blogCollection.findOne({ _id: slug });
-  if (item) {
-    if (regenerateVectors) {
-      const embeddings = await getEmbeddings(body);
-      await blogCollection.updateOne(
-        { _id: slug },
-        { $set: { $vector: embeddings } }
-      );
-      return embeddings;
-    }
-    return item.$vector;
-  }
-  const embeddings = await getEmbeddings(body);
-  await blogCollection.insertOne({ _id: slug, $vector: embeddings });
-  return embeddings;
-}
+const blogCollection = await astraDb.collection<BlogEmbeddingDoc>(
+  COLLECTION_NAME
+);
 
 function isPost(
   post: CollectionEntry<"blog"> | undefined
@@ -51,21 +31,22 @@ function isPost(
 export async function getRelatedPosts({
   slug,
   body,
-  regenerateVectors,
 }: BlogEmbeddingData): Promise<CollectionEntry<"blog">[]> {
-  const embeddings = await findOrCreateBlogEmbedding({
-    slug,
-    body,
-    regenerateVectors,
-  });
+  const blogContent = stringByteSlice(body, 0, 8000);
+
+  await blogCollection.findOneAndUpdate(
+    { _id: slug },
+    { $set: { $vectorize: blogContent } },
+    { upsert: true, returnDocument: "after" }
+  );
 
   const filter = { _id: { $ne: slug } };
-  const options = { sort: { $vector: embeddings }, limit: 4 };
+  const options = { sort: { $vectorize: blogContent }, limit: 4 };
 
-  const results = await blogCollection.find(filter, options).toArray();
+  const cursor = blogCollection.find(filter, options);
+  const results = await cursor.toArray();
   const posts = await Promise.all(
     results.map((result) => getEntry({ collection: "blog", slug: result._id }))
   );
-
   return posts.filter(isPost);
 }
