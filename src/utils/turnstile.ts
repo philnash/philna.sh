5287ad9bbe343ff4011da507f4c8e24f8f7ff565
Phil Nash <philnash@gmail.com>
2026-05-21 16:06:56 +0800

    Fixes npm check issues, generates wrangler types

--- src/utils/turnstile.ts
+++ src/utils/turnstile.ts
@@ -1,22 +1,26 @@
+interface SiteVerifyResponse {
+  success: boolean;
+}
+
 export async function validate(token: string, secret: string, ip: string) {
   const formData = new FormData();
-  formData.append('response', token);
-  formData.append('secret', secret);
-  formData.append('remoteip', ip);
+  formData.append("response", token);
+  formData.append("secret", secret);
+  formData.append("remoteip", ip);
 
-  const URL = 'https://challenges.cloudflare.com/turnstile/v0/siteverify';
+  const URL = "https://challenges.cloudflare.com/turnstile/v0/siteverify";
 
   const result = await fetch(URL, {
-		body: formData,
-		method: 'POST',
-	});
+    body: formData,
+    method: "POST",
+  });
 
-  const outcome = await result.json();
+  const outcome = await result.json() as SiteVerifyResponse;
 
   if (outcome.success) {
     return true;
   } else {
-    console.error(JSON.stringify(outcome))
+    console.error(JSON.stringify(outcome));
     return false;
   }
 }
